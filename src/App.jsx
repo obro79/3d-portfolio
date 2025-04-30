a7fe13575e99fc1618d08507998c5ea5aea141b6
owenfisher <owenfisher46@gmail.com>
2025-04-29 19:04:59 -0700

    setup/ initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1 className="text-3xl ">Welcome to my 3D Portfolio!</h1>
+            </div>
+        </main>
+    )
+}
+export default App
