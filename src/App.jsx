fbd4d59e6301d7459c71a1942411e3e4beae4a4d
owenfisher <owenfisher46@gmail.com>
2025-05-07 11:25:15 -0700

    Implement Room Model

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Hero from "./sections/Hero.jsx";
+
+
 
 const App = () => {
     return (
-        <main>
-            <div>
-                <h1 className="text-3xl ">Welcome to my 3D Portfolio!</h1>
-            </div>
-        </main>
+        <>
+            <Hero />
+        </>
     )
 }
 export default App
